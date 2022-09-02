6adae47892ea603a1a83c32c2b6fe4ff29095c2f
Avus <avuwake@gmail.com>
2022-09-02 19:31:50 +0100

    Done

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+//selecting UI components
+const openMenu = document.querySelector('#open-menu')
+const closeMenu = document.querySelector('#close-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+const toggleMenu = ()=> {
+    openMenu.classList.toggle('hidden')
+    closeMenu.classList.toggle('hidden') 
+    mobileMenu.classList.toggle('active')    
+}
+
+openMenu.addEventListener('click', toggleMenu)
+closeMenu.addEventListener('click', toggleMenu)
